faf4b2b74c928f5e1f30671c2472f1fce544f5c7
Ali <adamshels1@gmail.com>
2025-01-06 09:47:46 +0300

    fix: android build

--- src/shared/api/apiInstance.ts
+++ src/shared/api/apiInstance.ts
@@ -3,7 +3,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Создаем instance axios
 const apiInstance = axios.create({
-  baseURL: 'https://2493-176-88-21-146.ngrok-free.app/api', // Базовый URL для API
+  baseURL: 'http://13.61.4.143:3000/api', // Базовый URL для API
   timeout: 20000, // Таймаут запроса
   headers: {
     'Content-Type': 'application/json',
