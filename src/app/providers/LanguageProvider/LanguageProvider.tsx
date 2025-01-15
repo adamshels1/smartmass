9ed1e302c6df419e7ea9e7468ba451be7863eb49
Ali <adamshels1@gmail.com>
2025-01-15 19:57:22 +0300

    fix: LanguageProvider

--- src/app/providers/LanguageProvider/LanguageProvider.tsx
+++ src/app/providers/LanguageProvider/LanguageProvider.tsx
@@ -6,21 +6,22 @@ import 'moment/locale/es';
 import 'moment/locale/fr';
 import {FC, ReactNode, useEffect} from 'react';
 import i18n from 'shared/config/i18n';
+import {useSelector} from 'react-redux';
+import {RootState} from 'app/providers/StoreProvider';
 
 interface LanguageProviderProps {
   children: ReactNode;
 }
 
 export const LanguageProvider: FC<LanguageProviderProps> = ({children}) => {
-  // const language = useAppSelector(getLanguage);
-  const language = 'en';
+  const {user} = useSelector((state: RootState) => state.auth);
 
   useEffect(() => {
-    if (language) {
-      moment.locale(language);
-      i18n.changeLanguage(language);
+    if (user?.language) {
+      moment.locale(user?.language);
+      i18n.changeLanguage(user?.language);
     }
-  }, [language]);
+  }, [user?.language]);
 
   // Возвращаем элемент React
   return <>{children}</>;
